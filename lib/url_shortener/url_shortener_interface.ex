2936e96fcc805136e2d936fff74119cae751b5cf
Hassan Shaikley <hassan.shaikley@gmail.com>
2023-03-29 15:27:49 -0400

    Refactor business logic out of controller

---
+++ lib/url_shortener/url_shortener_interface.ex
@@ -0,0 +1,31 @@
+defmodule UrlShortener.UrlShortenerInterface do
+  require Logger
+
+  alias UrlShortener.Link
+
+  def create_or_fetch_link_by_url(url) do
+    case UrlShortener.Repo.get_by(Link, url: url) do
+      nil ->
+        Logger.notice("Doesn't exist, creating link")
+
+        changeset = Link.changeset(%Link{}, %{url: url, hits: 0})
+        UrlShortener.Repo.insert(changeset)
+
+      link ->
+        Logger.notice("Already exists, fetching link")
+
+        {:ok, link}
+    end
+  end
+
+  def get_link_by_short_url(short_url) do
+    case UrlShortener.Repo.get_by(Link, short_url: short_url) do
+      nil ->
+        nil
+
+      link ->
+        updated_link = Ecto.Changeset.change(link, hits: link.hits + 1)
+        UrlShortener.Repo.update!(updated_link)
+    end
+  end
+end
--- lib/url_shortener_web/controllers/url_shortener_controller.ex
+++ lib/url_shortener_web/controllers/url_shortener_controller.ex
@@ -1,26 +1,12 @@
 defmodule UrlShortenerWeb.UrlShortenerController do
   use UrlShortenerWeb, :controller
 
-  require Logger
-
-  alias UrlShortener.Link
+  alias UrlShortener.UrlShortenerInterface
 
   def create(conn, params) do
     url = params["url"]
 
-    link =
-      case UrlShortener.Repo.get_by(Link, url: url) do
-        nil ->
-          Logger.notice("Doesn't exist, creating link")
-
-          changeset = Link.changeset(%Link{}, %{url: url, hits: 0})
-          UrlShortener.Repo.insert(changeset)
-
-        link ->
-          Logger.notice("Already exists, fetching link")
-
-          {:ok, link}
-      end
+    link = UrlShortenerInterface.create_or_fetch_link_by_url(url)
 
     case link do
       {:error, _changeset} ->
@@ -36,16 +22,13 @@ defmodule UrlShortenerWeb.UrlShortenerController do
   def show(conn, params) do
     short_url = params["short_url"]
 
-    case UrlShortener.Repo.get_by(Link, short_url: short_url) do
+    case UrlShortenerInterface.get_link_by_short_url(short_url) do
       nil ->
         conn
         |> put_flash(:error, "Invalid short code, try regenerating")
         |> redirect(to: "/")
 
       link ->
-        updated_link = Ecto.Changeset.change(link, hits: link.hits + 1)
-        UrlShortener.Repo.update!(updated_link)
-
         redirect(conn, external: link.url)
     end
   end
