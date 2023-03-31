54def6361a638f264905d4c0df684716172d7405
Hassan Shaikley <hassan.shaikley@gmail.com>
2023-03-31 10:38:11 -0400

    remove redundant migration

--- priv/repo/migrations/20230330142855_make_url_and_short_url_unique.exs
+++
@@ -1,8 +0,0 @@
-defmodule UrlShortener.Repo.Migrations.MakeUrlAndShortUrlUnique do
-  use Ecto.Migration
-
-  def change do
-    create unique_index(:links, [:short_url])
-    create unique_index(:links, [:url])
-  end
-end
